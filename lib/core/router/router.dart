55ac87d45db2d3d2b3bcb9dea3a1dcf921436ca0
Aiswarya151 <aiswaryashaji55@gmail.com>
2025-04-02 15:58:40 +0530

    small changes

--- lib/core/router/router.dart
+++ lib/core/router/router.dart
@@ -1,7 +1,7 @@
 import 'package:cart_app/features/authentication/presentation/login.dart';
 import 'package:cart_app/features/authentication/presentation/signup.dart';
-import 'package:cart_app/features/cart/my_cart.dart';
-import 'package:cart_app/features/cart/products.dart';
+import 'package:cart_app/features/cart/presentation/my_cart.dart';
+import 'package:cart_app/features/cart/presentation/products.dart';
 import 'package:go_router/go_router.dart';
 
 final GoRouter appRouter=GoRouter(initialLocation:'/products',
